592d48867ad22db51fcec9eb1288111c619e0a0f
Rafael Weschle <rafael.weschle@gmail.com>
2023-01-20 16:49:11 +0100

    Support für React Query

--- ushell/src/App.jsx
+++ ushell/src/App.jsx
@@ -8,30 +8,35 @@ import "./App.css";
 import "./styles/main.less";
 
 import UShell from "./components/UShell";
+import { QueryClientProvider } from "react-query";
 
-const pickBasePath = ()=>{
-  let baseHref = (document.getElementsByTagName('base')[0] || {href:'/'}).href;
+const pickBasePath = () => {
+  let baseHref = (document.getElementsByTagName("base")[0] || { href: "/" })
+    .href;
 
   //Because of some strange magic, the browser automatically prepends the
   //hostname to the base-href. Also when reading the base-tag directly
   //from the html-head! So we need to remove the host to get the plain
   //configured value (a directory-url, relative to the root of the webserver)
-  baseHref = baseHref.replace("://","");
+  baseHref = baseHref.replace("://", "");
   let pos = baseHref.indexOf("/");
-  if(pos < 0){
+  if (pos < 0) {
     baseHref = "/";
-  }
-  else{
+  } else {
     baseHref = baseHref.substring(pos);
   }
- 
+
   return baseHref;
-}
+};
+
+const queryClient = new QueryClientProvider();
 
 const App = () => {
   return (
-      <BrowserRouter basename={ pickBasePath() }>
-      <UShell></UShell>
+    <BrowserRouter basename={pickBasePath()}>
+      <QueryClientProvider client={queryClient}>
+        <UShell></UShell>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 };
